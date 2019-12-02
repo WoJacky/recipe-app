eb152fc3034530e7bd1246e79f983d29bd774244
Wojciech Jackowiak <wojciechjackowiak26@gmail.com>
2019-12-02 15:32:09 +0100

    Updates

---
+++ precache-manifest.f3b0a4bf87c7e1de4a2b88315f485a1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe651c1aa2ffc93df9df45e899f1415",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "0db152dc76c675154045",
+    "url": "/recipe-app/static/css/main.a680d834.chunk.css"
+  },
+  {
+    "revision": "3b94ad5fd724215b1eec",
+    "url": "/recipe-app/static/js/2.be7a7059.chunk.js"
+  },
+  {
+    "revision": "0db152dc76c675154045",
+    "url": "/recipe-app/static/js/main.92994d12.chunk.js"
+  },
+  {
+    "revision": "5eaed5efb6700ea7011d",
+    "url": "/recipe-app/static/js/runtime-main.eef6e3ec.js"
+  }
+]);
